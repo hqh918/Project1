5b3976902375f67c9addffc7166b72bccd9d40ee
hqh <hqh_cn@outlook.com>
2024-03-01 20:37:07 +0800

    更行了获取时间功能，完成了初始化前的部分代码

---
+++ Project1/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+using namespace std;
+string Time(void);
+int main() {
+	cout << Time();
+	while (1);
+	string path_state = "Date/State.txt";
+	string path_log = "Date/log.txt";
+	ifstream fin;
+	ofstream fout;
+	fin.open(path_state, ios::in);
+	char state[10];
+	fin.getline(state, 10);
+	string str_state = state;
+	if (str_state == "Inactive") {
+		int size;
+		cout << "For the first time,initialization is required.\n";
+		cout << "Please enter the class size:";
+		cin >> size;
+		time_t timep;
+		time(&timep);
+
+	}
+	else {
+
+	}
+	return 0;
+}
+
+string Time(void) {
+	time_t timep;
+	time(&timep);
+	char tmp[32];
+	strftime(tmp, sizeof(tmp), "%Y-%m-%d %H:%M:%S", localtime(&timep));
+	string strtime = tmp;
+	return strtime;
+}
